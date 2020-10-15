a532862f4f3ea043a6b5eba90e75604123a6161a
Kelsey Busser <busserk@gmail.com>
2020-10-15 17:09:53 -0400

    latest add

--- script.js
+++ script.js
@@ -1,15 +1,61 @@
 
-$("#currentDay").text(moment().format('MMMM Do YYYY'));
+let m = moment();
 
-$(document).ready(function(){
+let currentTime = m.format("h:mma");
 
-    $(".saveBtn").on("click", function (){
-    var value = $(this).siblings(".description").val();
+let timeBlockContainer = document.getElementsByClassName("time-block");
+
+
+var nine = $("#9");
+var ten = $("#10");
+var eleven = $("#11");
+var twelve = $("#12");
+var thirteen = $("#13");
+var fourteen = $("#14");
+var fifteen = $("#15");
+var sixteen = $("#16");
+var seventeen = $("#17");
+
+function currentDate () {
+    $("#currentDay").text(moment().format("dddd MMMM Do YYYY"));
+    $("#currentTime").html("${currentTime}");
+}
+currentDate();
+
+$(".time-block").each(function(){
+    var id= $(this).attr("id");
+    var schedule = localStorage.getItem(id);
+    
+    if (schedule !== null) {
+        $(this).children(".schedule").val(schedule);
+    }
+});
+
+function changeColor() {
+
+    $(".time-block").each(function () {
+        hour = m.hours();
+        var thisHour = parseInt($(this).attr("id"));
+        if (thisHour > hour) {
+            $(this).addClass('future');
+
+        } else if (thisHour === hour) {
+            $(this).addClass('present');
+        } else {
+            $(this).addClass('past');
+        }
+     
+    });
+}
+changeColor();
+
+
+var saveBtn = $(".saveBtn");
+
+saveBtn.on("click", function () {
     var time = $(this).parent().attr("id");
-    localStorage.setItem(time, value);
-})
-})
+    var schedule = $(this).siblings(".schedule").val();
 
-
-
+    localStorage.setItem(time, schedule);
+});
 
